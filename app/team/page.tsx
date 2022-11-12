48b909ea4c9fe0ef761caf35040e6cc5880e7b9c
Shriprasanna B <shriprasanna32@gmail.com>
2022-11-12 11:54:55 +0530

    team full done

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -13,36 +13,39 @@ const Team: FC = () => {
         Meet the <span className="text-sky-500">Team</span>
       </h1>
 
-      <div className="flex justify-center items-center ">
-        <main className="border border-white/50 w-[350px] h-[180px] rounded-md  ">
-          <section className="border border-sky-500/30 h-[120px] bg-white/30 ">
-            <h1 className="text-white float-right mt-[79px] mr-6 text-2xl font-medium font-sp">
+      <div className="flex justify-center items-center group">
+        <main className="border-2 border-white/50 w-[350px] h-[180px] rounded-md ">
+          <section className=" h-[120px] bg-white/40 group-hover:bg-gradient-to-r from-cyan-500 to-blue-500 ">
+            <h1
+              className="text-white float-right mt-[79px] mr-6 text-2xl font-medium font-sp"
+              title="name"
+            >
               Shriprasanna
             </h1>
           </section>
 
           <div className="flex gap-[50px]">
             <div
-              className="w-[130px] relative border border-green-400 h-[130px] rounded-full 
+              className="w-[135px] relative border border-white/40 h-[135px] rounded-full 
           -top-[100px] left-5 overflow-hidden"
             >
               <Image
                 src={mee}
                 alt="images"
-                className="aspect-square"
+                className="aspect-square grayscale-[90%] group-hover:grayscale-0 transition-all duration-300"
                 priority
               />
             </div>
 
-            <div className="text-white mt-4 flex text-[28px] gap-[20px] mr-2">
-              <Link href="/">
-                <CiTwitter className="text-4xl -mt-1" />
+            <div className="text-white mt-4 flex text-[28px] gap-[20px] mr-2 transition-all duration-500">
+              <Link href="/" target="blank" title="Twitter">
+                <CiTwitter className="text-4xl -mt-1 hover:text-sky-500" />
               </Link>
-              <Link href="/">
-                <VscGithubAlt />
+              <Link href="/" target="blank" title="github">
+                <VscGithubAlt className=" hover:text-sky-500" />
               </Link>
-              <Link href="/">
-                <SlSocialLinkedin />
+              <Link href="/" target="blank" title="github">
+                <SlSocialLinkedin className=" hover:text-sky-500" />
               </Link>
             </div>
           </div>
