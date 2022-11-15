a7b3ee17c3ef4dea6ef6a29e6f5ce1c5949dc0c1
Shriprasanna <92677078+shrix1@users.noreply.github.com>
2022-11-15 22:59:03 +0530

    Update Footer.tsx

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -3,12 +3,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-sky-500/80 p-4 font-pop flex justify-around items-center">
-      <Link href="/" className="text-2xl">
-        IBM
+   <footer className="bg-sky-500/80 p-4 font-pop flex justify-around items-center">
+      <Link href="/" className="text-xl">
+        Analytics for Hospitals <br className="md:hidden " />
+        Health-Care Data
       </Link>
 
-      <h1>
+      <h1 className="flex flex-col">
         TEAM ID : <strong>PNT2022TMID39750</strong>
       </h1>
     </footer>
