16c6907f4191985e26667cd2e243c7aa8bf9d4a6
Shriprasanna B <shriprasanna32@gmail.com>
2022-11-19 18:59:15 +0530

    team page fixed

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -5,6 +5,10 @@ import { SlSocialLinkedin } from "react-icons/sl";
 import { CiTwitter } from "react-icons/ci";
 import Image from "next/image";
 import { TeamDetails } from "./TeamDetails";
+import shri from ".//shri.jpg";
+import kiran from ".//kiran.jpeg";
+import praveen from ".//praveen.jpg";
+import ram from ".//ram.jpg";
 
 const Team: FC = () => {
   return (
@@ -14,64 +18,208 @@ const Team: FC = () => {
       </h1>
 
       <section className="flex justify-center items-center mt-10 ">
-        <main className="flex flex-wrap  justify-evenly items-center w-[1000px] md:h-[60vh] ">
-          {TeamDetails.map((i, idx) => {
-            return (
-              <div className="flex justify-around items-center group" key={idx}>
-                <main className="border-2 border-white/50 w-[350px] h-[180px] rounded-md mt-[10px]">
-                  <section className=" h-[120px] bg-white/40 group-hover:bg-gradient-to-r from-cyan-500 to-blue-500 ">
-                    <h1
-                      className="text-white float-right mt-[79px] mr-6 text-2xl font-medium font-sp"
-                      title="name"
-                    >
-                      {i.name}
-                    </h1>
-                  </section>
+        <main className="flex flex-wrap justify-evenly items-center w-[1000px] md:h-[60vh] ">
+          <div className="flex justify-around items-center group flex-wrap">
+            {/* 1 */}
+            <main className="border-2 border-white/50 w-[350px] h-[180px] rounded-md mt-[10px]">
+              <section className=" h-[120px] bg-white/40 group-hover:bg-gradient-to-r from-cyan-500 to-blue-500 ">
+                <h1
+                  className="text-white float-right mt-[79px] mr-6 text-2xl font-medium font-sp"
+                  title="name"
+                >
+                  Shriprasanna
+                </h1>
+              </section>
 
-                  <div className="flex gap-[50px]">
-                    <div
-                      className="w-[135px] relative border border-white/40 h-[135px] rounded-full 
+              <div className="flex gap-[50px]">
+                <div
+                  className="w-[135px] relative border border-white/40 h-[135px] rounded-full 
                       -top-[100px] left-5 overflow-hidden"
-                    >
-                      <Image
-                        src={i.photo}
-                        alt="images"
-                        className="aspect-square transition-all duration-300"
-                        priority
-                      />
-                    </div>
+                >
+                  <Image
+                    src={shri}
+                    alt="images"
+                    className="aspect-square transition-all duration-300"
+                    priority
+                  />
+                </div>
 
-                    <div className="text-white mt-4 flex text-[28px] gap-[20px] mr-2 transition-all duration-500">
-                      <Link
-                        // href={i.twitter}
-                        href="https://twitter.com/"
-                        target="blank"
-                        title="Twitter"
-                      >
-                        <CiTwitter className="text-4xl -mt-1 hover:text-sky-500" />
-                      </Link>
-                      <Link
-                        // href={i.github}
-                        href="https://github.com/"
-                        target="blank"
-                        title="github"
-                      >
-                        <VscGithubAlt className=" hover:text-sky-500" />
-                      </Link>
-                      <Link
-                        // href={i.linkedin}
-                        href="https://www.linkedin.com/in/"
-                        target="blank"
-                        title="github"
-                      >
-                        <SlSocialLinkedin className=" hover:text-sky-500" />
-                      </Link>
-                    </div>
-                  </div>
-                </main>
+                <div className="text-white mt-4 flex text-[28px] gap-[20px] mr-2 transition-all duration-500">
+                  <Link
+                    href="https://twitter.com/shriprasanna007"
+                    target="blank"
+                    title="Twitter"
+                  >
+                    <CiTwitter className="text-4xl -mt-1 hover:text-sky-500" />
+                  </Link>
+                  <Link
+                    href="https://github.com/shrix1"
+                    target="blank"
+                    title="github"
+                  >
+                    <VscGithubAlt className=" hover:text-sky-500" />
+                  </Link>
+                  <Link
+                    href="https://www.linkedin.com/in/shriprasanna-b-62aab1208/"
+                    target="blank"
+                    title="github"
+                  >
+                    <SlSocialLinkedin className=" hover:text-sky-500" />
+                  </Link>
+                </div>
               </div>
-            );
-          })}
+            </main>
+
+            {/* 2 */}
+            <main className="border-2 border-white/50 w-[350px] h-[180px] rounded-md mt-[10px]">
+              <section className=" h-[120px] bg-white/40 group-hover:bg-gradient-to-r from-cyan-500 to-blue-500 ">
+                <h1
+                  className="text-white float-right mt-[79px] mr-6 text-2xl font-medium font-sp"
+                  title="name"
+                >
+                  KiranKumar
+                </h1>
+              </section>
+
+              <div className="flex gap-[50px]">
+                <div
+                  className="w-[135px] relative border border-white/40 h-[135px] rounded-full 
+                      -top-[100px] left-5 overflow-hidden"
+                >
+                  <Image
+                    src={kiran}
+                    alt="images"
+                    className="aspect-square transition-all duration-300"
+                    priority
+                  />
+                </div>
+
+                <div className="text-white mt-4 flex text-[28px] gap-[20px] mr-2 transition-all duration-500">
+                  <Link
+                    href="https://twitter.com/its_k_orb?t=WzN7x-uiIOQfCgv4jsgFsA&s=09"
+                    target="blank"
+                    title="Twitter"
+                  >
+                    <CiTwitter className="text-4xl -mt-1 hover:text-sky-500" />
+                  </Link>
+                  <Link
+                    href="https://github.com/kiran0022"
+                    target="blank"
+                    title="github"
+                  >
+                    <VscGithubAlt className=" hover:text-sky-500" />
+                  </Link>
+                  <Link
+                    href="https://www.linkedin.com/in/kiran-kumar-k-61ba89203"
+                    target="blank"
+                    title="github"
+                  >
+                    <SlSocialLinkedin className=" hover:text-sky-500" />
+                  </Link>
+                </div>
+              </div>
+            </main>
+
+            {/* 3  */}
+            <main className="border-2 border-white/50 w-[350px] h-[180px] rounded-md mt-[10px]">
+              <section className=" h-[120px] bg-white/40 group-hover:bg-gradient-to-r from-cyan-500 to-blue-500 ">
+                <h1
+                  className="text-white float-right mt-[79px] mr-6 text-2xl font-medium font-sp"
+                  title="name"
+                >
+                  Praveen
+                </h1>
+              </section>
+
+              <div className="flex gap-[50px]">
+                <div
+                  className="w-[135px] relative border border-white/40 h-[135px] rounded-full 
+                      -top-[100px] left-5 overflow-hidden"
+                >
+                  <Image
+                    src={praveen}
+                    alt="images"
+                    className="aspect-square transition-all duration-300"
+                    priority
+                  />
+                </div>
+
+                <div className="text-white mt-4 flex text-[28px] gap-[20px] mr-2 transition-all duration-500">
+                  <Link
+                    href="https://twitter.com/"
+                    target="blank"
+                    title="Twitter"
+                  >
+                    <CiTwitter className="text-4xl -mt-1 hover:text-sky-500" />
+                  </Link>
+                  <Link
+                    href="https://github.com/praveendecode"
+                    target="blank"
+                    title="github"
+                  >
+                    <VscGithubAlt className=" hover:text-sky-500" />
+                  </Link>
+                  <Link
+                    href="https://www.linkedin.com/in/praveen-n-2b4004223"
+                    target="blank"
+                    title="github"
+                  >
+                    <SlSocialLinkedin className=" hover:text-sky-500" />
+                  </Link>
+                </div>
+              </div>
+            </main>
+
+            {/* 4 */}
+            <main className="border-2 border-white/50 w-[350px] h-[180px] rounded-md mt-[10px]">
+              <section className=" h-[120px] bg-white/40 group-hover:bg-gradient-to-r from-cyan-500 to-blue-500 ">
+                <h1
+                  className="text-white float-right mt-[79px] mr-6 text-2xl font-medium font-sp"
+                  title="name"
+                >
+                  RamSanjay
+                </h1>
+              </section>
+
+              <div className="flex gap-[50px]">
+                <div
+                  className="w-[135px] relative border border-white/40 h-[135px] rounded-full 
+                      -top-[100px] left-5 overflow-hidden"
+                >
+                  <Image
+                    src={ram}
+                    alt="images"
+                    className="aspect-square transition-all duration-300"
+                    priority
+                  />
+                </div>
+
+                <div className="text-white mt-4 flex text-[28px] gap-[20px] mr-2 transition-all duration-500">
+                  <Link
+                    href="https://twitter.com/ram_sanjay_8?t=_Pnt_oTBgkr42UR8hw6bgA&s=09"
+                    target="blank"
+                    title="Twitter"
+                  >
+                    <CiTwitter className="text-4xl -mt-1 hover:text-sky-500" />
+                  </Link>
+                  <Link
+                    href="https://github.com/RamSanjay08"
+                    target="blank"
+                    title="github"
+                  >
+                    <VscGithubAlt className=" hover:text-sky-500" />
+                  </Link>
+                  <Link
+                    href="https://www.linkedin.com/in/ram-sanjay-076769227"
+                    target="blank"
+                    title="github"
+                  >
+                    <SlSocialLinkedin className=" hover:text-sky-500" />
+                  </Link>
+                </div>
+              </div>
+            </main>
+          </div>
           ;
         </main>
       </section>
